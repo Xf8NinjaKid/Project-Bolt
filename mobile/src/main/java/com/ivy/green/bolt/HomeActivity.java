01b8cfc744e8b62c2351ea20754130f22f30c9c1
Bibaswan Bhawal <bibaswan36000@outlook.com>
2016-11-01 10:04:06 +0200

    Alpha 1 update

--- mobile/src/main/java/com/ivy/green/bolt/HomeActivity.java
+++ mobile/src/main/java/com/ivy/green/bolt/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.ivy.green.bolt;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,7 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+
+
 public class HomeActivity extends AppCompatActivity {
+
+    public static final String PREFS_NAME = "LogData";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +27,18 @@ public class HomeActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Signed-out", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                FirebaseAuth.getInstance().signOut();
+
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean("log", false);
+                editor.putString("Email", null);
+                editor.putString("Password", null);
+
+                editor.apply();
+
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
